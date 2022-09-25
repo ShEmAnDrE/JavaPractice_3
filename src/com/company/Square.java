78f441d860ab124bb26119001963d3b87838e22f
Андрей Шемчук <andreyshemchuk@MacBook-Pro-Andrej.local>
2022-09-25 22:24:20 +0300

    Initial commit

---
+++ src/com/company/Square.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square (double side, String color, boolean filled) {
+        super(side, side, color, filled);
+    }
+
+    public double getSide() {
+        return getLength();
+    }
+
+    public void setSide(double side) {
+        setLength(side);
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square: side = " + getSide() + ", color = " + color;
+    }
+}
